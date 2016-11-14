bab5762a40dc96a46e823038c1618d9f15e5a793
infernalslam <infernal-slam@hotmail.com>
2016-11-15 02:34:15 +0700

    cors

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
+var cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json())
+
 app.set('port', (process.env.PORT || 4000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
 
 app.get('/', function (req, res) {
 	res.send('Hello')
